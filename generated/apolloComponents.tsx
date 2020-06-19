655e5476887ad9e8a07fc993032f97f57125542f
John <jjh5166@gmail.com>
2020-06-18 23:16:50 -0400

    codegen

---
+++ generated/apolloComponents.tsx
@@ -0,0 +1,198 @@
+import gql from 'graphql-tag';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as React from 'react';
+import * as ApolloReactComponents from '@apollo/react-components';
+import * as ApolloReactHoc from '@apollo/react-hoc';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  me?: Maybe<User>;
+  hello: Scalars['String'];
+  books: Array<Book>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type Book = {
+  __typename?: 'Book';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  authors: Array<Author>;
+};
+
+export type Author = {
+  __typename?: 'Author';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  changePassword?: Maybe<User>;
+  confirmUser: Scalars['Boolean'];
+  forgotPassword: Scalars['Boolean'];
+  login?: Maybe<User>;
+  logout: Scalars['Boolean'];
+  register: User;
+  createProduct: Product;
+  addProfilePicture: Scalars['Boolean'];
+  createUser: User;
+  createBook: Book;
+  createAuthor: Author;
+  addAuthorBook: Scalars['Boolean'];
+  deleteBook: Scalars['Boolean'];
+};
+
+
+export type MutationChangePasswordArgs = {
+  data: ChangePasswordInput;
+};
+
+
+export type MutationConfirmUserArgs = {
+  token: Scalars['String'];
+};
+
+
+export type MutationForgotPasswordArgs = {
+  email: Scalars['String'];
+};
+
+
+export type MutationLoginArgs = {
+  password: Scalars['String'];
+  email: Scalars['String'];
+};
+
+
+export type MutationRegisterArgs = {
+  data: RegisterInput;
+};
+
+
+export type MutationCreateProductArgs = {
+  data: ProductInput;
+};
+
+
+export type MutationAddProfilePictureArgs = {
+  picture: Scalars['Upload'];
+};
+
+
+export type MutationCreateUserArgs = {
+  data: RegisterInput;
+};
+
+
+export type MutationCreateBookArgs = {
+  name: Scalars['String'];
+};
+
+
+export type MutationCreateAuthorArgs = {
+  name: Scalars['String'];
+};
+
+
+export type MutationAddAuthorBookArgs = {
+  bookId: Scalars['Int'];
+  authorId: Scalars['Int'];
+};
+
+
+export type MutationDeleteBookArgs = {
+  bookId: Scalars['Int'];
+};
+
+export type ChangePasswordInput = {
+  password: Scalars['String'];
+  token: Scalars['String'];
+};
+
+export type RegisterInput = {
+  password: Scalars['String'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
+};
+
+export type Product = {
+  __typename?: 'Product';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
+export type ProductInput = {
+  name: Scalars['String'];
+};
+
+
+export type LoginMutationVariables = Exact<{
+  email: Scalars['String'];
+  password: Scalars['String'];
+}>;
+
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { login?: Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'firstName' | 'lastName' | 'email' | 'name'>
+  )> }
+);
+
+
+export const LoginDocument = gql`
+    mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    id
+    firstName
+    lastName
+    email
+    name
+  }
+}
+    `;
+export type LoginMutationFn = ApolloReactCommon.MutationFunction<LoginMutation, LoginMutationVariables>;
+export type LoginComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<LoginMutation, LoginMutationVariables>, 'mutation'>;
+
+    export const LoginComponent = (props: LoginComponentProps) => (
+      <ApolloReactComponents.Mutation<LoginMutation, LoginMutationVariables> mutation={LoginDocument} {...props} />
+    );
+    
+export type LoginProps<TChildProps = {}, TDataName extends string = 'mutate'> = {
+      [key in TDataName]: ApolloReactCommon.MutationFunction<LoginMutation, LoginMutationVariables>
+    } & TChildProps;
+export function withLogin<TProps, TChildProps = {}, TDataName extends string = 'mutate'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  LoginMutation,
+  LoginMutationVariables,
+  LoginProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withMutation<TProps, LoginMutation, LoginMutationVariables, LoginProps<TChildProps, TDataName>>(LoginDocument, {
+      alias: 'login',
+      ...operationOptions
+    });
+};
+export type LoginMutationResult = ApolloReactCommon.MutationResult<LoginMutation>;
+export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
