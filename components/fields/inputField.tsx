22aa8e49b951955ecb0540119828fc0eb2fb0c37
John <jjh5166@gmail.com>
2020-06-20 11:20:12 -0400

    formik errors

--- components/fields/inputField.tsx
+++ components/fields/inputField.tsx
@@ -3,10 +3,16 @@ import { FieldProps } from 'formik';
 
 type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 // custom input field
-export const InputField = ({ field, form: _, ...props }: FieldProps & InputProps) => {
+export const InputField = ({
+  field,
+  form: { errors, touched },
+  ...props
+}: FieldProps & InputProps) => {
+  const errorMessage = touched[field.name] && errors[field.name];
   return (
     <div>
       <input {...field} {...props} />
+      {errorMessage && <div style={{color: "red"}}>{errorMessage}</div>}
     </div>
   )
 }
